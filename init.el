3df77101d6f20b0488fd6f5848c1ad282d2be0a9
dreperson <151375814+dreperson@users.noreply.github.com>
2025-06-14 22:00:17 -0400

    Refactor init.el for cleaner startup

--- init.el
+++ init.el
@@ -1,30 +1,37 @@
-;; init.el -- Cleaned up Emacs configuration
+;; init.el --- Cleaned and organized Emacs configuration -*- lexical-binding: t; -*-
 
-;;; Basic startup settings
-(server-start)                               ; Start server for emacsclient
-(setq inhibit-startup-message t              ; Disable startup screen
-      visible-bell t                         ; Flash instead of beep
-      use-dialog-box nil)                    ; Disable dialog boxes
+;;; Commentary:
+;; Minimal init file which loads packages with `use-package`.
 
-;; Store custom variables in separate file
-(setq custom-file (locate-user-emacs-file "custom.el"))
-(load custom-file t)
+;;; Code:
+
+(require 'server)
+(unless (server-running-p)
+  (server-start))
+
+(setq inhibit-startup-message t
+      visible-bell t
+      use-dialog-box nil)
+
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file t))
 
 ;;; Interface tweaks
 (menu-bar-mode -1)
 (tool-bar-mode -1)
-(tooltip-mode -1)
 (scroll-bar-mode -1)
+(tooltip-mode -1)
 (set-fringe-mode 10)
 (global-visual-line-mode 1)
 (add-to-list 'default-frame-alist '(font . "Iosevka-22"))
 (load-theme 'misterioso t)
 
-;;; File and buffer behavior
-(recentf-mode 1)                              ; Keep list of recent files
-(save-place-mode 1)                           ; Remember last visited location
-(windmove-default-keybindings)                ; Shift+arrows to move between windows
-(global-auto-revert-mode 1)                   ; Reload files changed on disk
+;;; File handling
+(recentf-mode 1)
+(save-place-mode 1)
+(windmove-default-keybindings)
+(global-auto-revert-mode 1)
 (setq global-auto-revert-non-file-buffers t)
 
 ;;; Package management
@@ -35,6 +42,9 @@
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
 (require 'use-package)
 (setq use-package-always-ensure t)
@@ -48,58 +58,63 @@
   (auto-package-update-maybe)
   (auto-package-update-at-time "09:00"))
 
-(use-package no-littering)
-(setq auto-save-file-name-transforms
-      (list (list ".*" (no-littering-expand-var-file-name "auto-save/") t)))
-
-;;; User interface packages
-(use-package command-log-mode)
-(use-package nerd-icons
+(use-package no-littering
   :config
-  (nerd-icons-install-fonts t))
+  (setq auto-save-file-name-transforms
+        `((".*" ,(no-littering-expand-var-file-name "auto-save/") t))))
+
+;;; UI packages
+(use-package command-log-mode)
+
+(use-package nerd-icons
+  :config (nerd-icons-install-fonts t))
+
 (use-package doom-modeline
   :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 15)))
+  :custom (doom-modeline-height 15))
+
 (use-package which-key
   :init (which-key-mode)
   :diminish which-key-mode
-  :config (setq which-key-idle-delay 1))
+  :custom (which-key-idle-delay 1))
+
 (use-package ivy
   :diminish
-  :init
-  (setq ivy-re-builders-alist '((t . ivy--regex-fuzzy)))
-  :config
-  (ivy-mode 1))
+  :init (setq ivy-re-builders-alist '((t . ivy--regex-fuzzy)))
+  :config (ivy-mode 1))
+
 (use-package counsel
   :after ivy
-  :config
-  (counsel-mode 1))
+  :config (counsel-mode 1))
+
 (use-package general)
+
 (use-package projectile
   :diminish projectile-mode
   :config
   (projectile-mode 1)
-  (setq projectile-completion-system 'auto)
-  (setq projectile-enable-caching t)
-  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
+  (setq projectile-completion-system 'auto
+        projectile-enable-caching t)
+  (define-key projectile-mode-map (kbd "C-c p") #'projectile-command-map))
+
 (use-package dashboard
   :init
-  (setq dashboard-banner-logo-title "okayyy let's go")
-  (setq dashboard-startup-banner 'logo)
-  (setq dashboard-center-content t)
-  (setq dashboard-vertically-center-content t)
-  (setq dashboard-display-icons-p t)
-  (setq dashboard-icon-type 'nerd-icons)
-  (setq dashboard-items '((recents  . 5)
+  (setq dashboard-banner-logo-title "okayyy let's go"
+        dashboard-startup-banner 'logo
+        dashboard-center-content t
+        dashboard-vertically-center-content t
+        dashboard-display-icons-p t
+        dashboard-icon-type 'nerd-icons
+        dashboard-items '((recents  . 5)
                           (bookmarks . 5)
                           (projects . 5)
-                          (agenda . 5)))
-  (setq initial-buffer-choice (lambda ()
+                          (agenda . 5))
+        initial-buffer-choice (lambda ()
                                 (get-buffer-create dashboard-buffer-name)))
   :config
   (dashboard-setup-startup-hook))
 
-;;; Org-journal configuration
+;;; Org-journal
 (use-package org-journal
   :custom
   (org-journal-dir (expand-file-name "~/Documents/Journal/"))
@@ -107,6 +122,7 @@
   (org-journal-date-format "%B %d, %Y")
   (org-journal-file-header "%B %d, %Y\n\n"))
 
+;;; Key bindings
 (general-create-definer my-leader-def
   :states '(normal visual emacs)
   :prefix "SPC")
@@ -118,7 +134,7 @@
   "p" '(projectile-command-map :which-key "projectile")
   "f f" '(counsel-find-file :which-key "find file"))
 
-;;; Evil mode
+;;; Evil
 (use-package evil
   :demand t
   :init
@@ -127,17 +143,15 @@
   :config
   (evil-mode 1))
 
-;;; Evil Collection - additional keybindings for Evil mode
 (use-package evil-collection
   :after evil
   :demand t
   :custom (evil-collection-mode-list 'all)
-  :config
-  (evil-collection-init))
+  :config (evil-collection-init))
 
 ;;; Global key bindings
-(global-set-key (kbd "C-x r") 'recentf-open-files)
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(global-set-key (kbd "C-x r") #'recentf-open-files)
+(global-set-key (kbd "<escape>") #'keyboard-escape-quit)
 
 ;;; Spell checking
 (use-package flyspell
@@ -148,9 +162,12 @@
                                 (executable-find "hunspell"))
         ispell-dictionary "en_US"))
 
+;;; PDF tools
 (use-package pdf-tools
+  :mode ("\\.pdf\\'" . pdf-view-mode)
   :config
   (pdf-tools-install)
-  (setq-default pdf-view-display-size 'fit-page)
-  (add-to-list 'auto-mode-alist '("\\.pdf\\'" . pdf-view-mode)))
+  (setq-default pdf-view-display-size 'fit-page))
 
+(provide 'init)
+;;; init.el ends here
